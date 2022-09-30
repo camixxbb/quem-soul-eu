b601c1c986369000e0091530e8e24790f25d11ac
camixxbb <camillavamp13@gmail.com>
2022-09-30 10:58:46 -0300

    first commit

---
+++ formcontatojava.js
@@ -0,0 +1,9 @@
+function Enviar() {
+
+    var nome = document.getElementById("nomeid");
+
+    if (nome.value != "") {
+        alert('Obrigada queen ' + nome.value + '! Em breve entraremos em contato');
+    }
+
+}
